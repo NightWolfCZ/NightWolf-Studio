fb37afe305a5b1a5a7c47b823010ddbec24b4669
NightWolfCZ <85906270+NightWolfCZ@users.noreply.github.com>
2026-03-22 12:19:30 +0100

    Add files via upload
    Redesign page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  integrations: [react()],
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+// https://astro.build/config
+export default defineConfig({});
